ec4b411e03ae11f1914d1a62474e62e0f7f19d5d
hananabilabd <hananabil@live.com>
2018-07-06 16:45:35 +0200

    Finalize

--- EMG_model.py
+++ EMG_model.py
@@ -2,7 +2,14 @@ from sklearn.externals import joblib
 
 class EMG_Model():
 
-    def prepare_data(self,intended_movement_labels,rows=8000):
+    def prepare_data(self,path1 ,path2,path3,path4,intended_movement_labels=[0,1,2,3],rows=8000):
+        emg_set = {}
+
+        emg_set[0] = pd.read_csv( path1, header=None )
+        emg_set[1] = pd.read_csv( path2, header=None )
+        emg_set[2] = pd.read_csv( path3, header=None )
+        emg_set[3] = pd.read_csv( path4, header=None )
+        rows =min (emg_set[0].shape[0],emg_set[1].shape[0],emg_set[2].shape[0],emg_set[3].shape[0])
 
         rep = []
         reps =rows // 6 if rows % 6 == 0 else (rows //6)+1
@@ -12,10 +19,10 @@ class EMG_Model():
                 rep.append(i)
 
         rep = rep[:rows]
-        emg_set = {}
+
 
         for i in intended_movement_labels:
-            emg_set[i] = pd.read_csv('models/' +str(i)+".csv" ,nrows =rows,header=None)
+            #emg_set[i] = pd.read_csv('models/' +str(i)+".csv" ,nrows =rows,header=None)
             emg_set[i]['label'] = i
             emg_set[i].columns = [1,2,3,4,5,6,7,8,'label']
             emg_set[i]['rep'] = rep
@@ -65,7 +72,6 @@ class EMG_Model():
 
 
     def all_steps(self,movements,file_name):
-        from sklearn.externals import joblib
 
         predictors_train,outcomes_train,predictors_test,outcomes_test = self.prepare_data(movements)
         model = self.svm_model(predictors_train,outcomes_train)
